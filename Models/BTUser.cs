f654b1d92ff1545c8b5ea189e67194bd30563eb4
TJF71 <138714842+TJF71@users.noreply.github.com>
2023-12-13 17:48:03 -0500

    Add project files.

---
+++ Models/BTUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace TroubleTrails.Models
+{
+    public class BTUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Full Name")]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }  //concatenates first and last name from db 
+    }
+}
