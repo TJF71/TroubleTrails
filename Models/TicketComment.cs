83cd0090cd683a0d24d9a13af05f2b739046e458
TJF71 <138714842+TJF71@users.noreply.github.com>
2024-01-05 17:45:30 -0500

    first attempt at TicketComments

---
+++ Models/TicketComment.cs
@@ -0,0 +1,34 @@
+﻿namespace TroubleTrails.Models
+{
+    public class TicketComment
+    {
+        //Id
+        public int Id { get; set; }
+
+        //Comment
+
+        public string Comment { get; set; }
+
+        //Created
+
+        public DateTimeOffset Created { get; set; } // when the comment was made
+
+        //TicketId
+
+        public int TicketId { get; set; } // FK which references the ticket table   
+
+        //UserId
+
+        public string UserId { get; set; } // FK which references the user table
+
+        //--Navigation Properties--//
+
+        //Ticket
+        public virtual Ticket Ticket { get; set; } // navigation property to the ticket table   
+        //User
+        public virtual BTUser User { get; set; } // navigation property to the user table   
+
+
+
+    }
+}
