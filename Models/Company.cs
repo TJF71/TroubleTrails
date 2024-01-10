20afc0e07f5e7db8400c2ec64b0beb349927acb3
TJF71 <138714842+TJF71@users.noreply.github.com>
2024-01-10 18:04:48 -0500

    Added Company model

---
+++ Models/Company.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace TroubleTrails.Models
+{
+    public class Company
+    {
+
+        public int Id { get; set; } // PK
+
+        [DisplayName("Company Name")]
+        public string Name { get; set; } // name of the company
+
+        [DisplayName("Company Description")]
+        public string Description { get; set; } // description of the company
+
+        //Navigation Properties
+        public virtual ICollection<BTUser> Members { get; set; } = new HashSet<BTUser>(); // navigation property to the BTUser table  lazy loading
+
+        public virtual ICollection<Project> Projects { get; set; } = new HashSet<Project>(); // navigation property to the project table lazy loading
+
+
+
+
+
+    }
+}
