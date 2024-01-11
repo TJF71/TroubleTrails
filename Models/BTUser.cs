23875029f79c578068044a04bed04d7f6986fea4
TJF71 <138714842+TJF71@users.noreply.github.com>
2024-01-10 20:46:01 -0500

    additional updates to BTUser

--- Models/BTUser.cs
+++ Models/BTUser.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Reflection.Metadata.Ecma335;
 
 namespace TroubleTrails.Models
 {
-    public class BTUser : IdentityUser //btuser inherits from identityuser
+    public class BTUser : IdentityUser //btuser inherits from identityuser and extends it with the properties below
     {
         [Required]
         [Display(Name = "First Name")]
@@ -18,5 +19,25 @@ namespace TroubleTrails.Models
         [NotMapped]
         [Display(Name = "Full Name")]
         public string FullName { get { return $"{FirstName} {LastName}"; } }  //concatenates first and last name from db 
+
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile AvatarFormFile { get; set; }
+
+        [DisplayName("Avatar")]
+        public string AvatarFileName { get; set; }
+        public byte[] AvatarFileData { get; set; } 
+
+        [Display(Name = "File Extension")]
+        public string AvatarContentType { get; set; } 
+
+       public int? CompanyID { get; set; } //FK to the company table
+
+        //Navigation Properties
+        public virtual Company Company { get; set; } // navigation property to the company table
+
+        public virtual ICollection<Project> Projects { get; set; } // join table between btuser and project
+
     }
 }
