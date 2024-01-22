7a44242129f8e076e1a0ab3de247b7aed476a7c2
TJF71 <138714842+TJF71@users.noreply.github.com>
2024-01-22 18:20:38 -0500

    completed scaffoldoing of controllers and views

---
+++ Controllers/CompaniesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TroubleTrails.Data;
+using TroubleTrails.Models;
+
+namespace TroubleTrails.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CompaniesController(ApplicationDbContext context)  // constructor  for the controller that takes in the dbcontext
+        {
+            _context = context;
+        }
+
+        // GET: Companies
+        public async Task<IActionResult> Index()
+        {
+              return _context.Companies != null ? 
+                          View(await _context.Companies.ToListAsync()) :  // if the companies table is not null, return the view with the list of companies
+                          Problem("Entity set 'ApplicationDbContext.Companies'  is null.");  // if the companies table is null, return a problem
+        }
+
+        // GET: Companies/Details/5    
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Companies == null) // if the id is null or the companies table is null, return not found
+            {
+                return NotFound();
+            }
+
+            var company = await _context.Companies
+                .FirstOrDefaultAsync(m => m.Id == id);  // find the company with the id passed in from the db context  and store it in the company variable     
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);  // return the view with the company
+        }
+
+        // GET: Companies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Companies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(company);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
+        }
+
+        // GET: Companies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Companies == null)
+            {
+                return NotFound();
+            }
+
+            Company company = await _context.Companies.FindAsync(id); // find the company with the id passed in from the db context  and store it in the company variable
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return View(company);  // return the view with the company
+        }
+
+        // POST: Companies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Company company)
+        {
+            if (id != company.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(company);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CompanyExists(company.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
+        }
+
+        // GET: Companies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Companies == null)
+            {
+                return NotFound();
+            }
+
+            var company = await _context.Companies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
+        }
+
+        // POST: Companies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Companies == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Companies'  is null.");
+            }
+            var company = await _context.Companies.FindAsync(id);
+            if (company != null)
+            {
+                _context.Companies.Remove(company);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CompanyExists(int id)
+        {
+          return (_context.Companies?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
