7a55a3fadac02eb951d566be61eaeedbadf5e409
TJF71 <138714842+TJF71@users.noreply.github.com>
2024-02-06 14:25:39 -0500

    added CompayInfoService Interface

---
+++ Services/Interfaces/IBTCompanyInfoService.cs
@@ -0,0 +1,12 @@
+﻿using TroubleTrails.Models;
+
+namespace TroubleTrails.Services.Interfaces
+{
+    public interface IBTCompanyInfoService
+    {
+       public Task<Company> GetCompanyInfoByIdAsync(int? companyId); // first method to get company info by id ? = nullable
+        public Task<List<BTUser>> GetAllMemebersAsync(int companyId); // second method to get all members of a company
+        public Task<List<Project>> GetAllProjectsAsync(int companyId);
+        public Task<List<Ticket>> GetAllTicketsAsync(int companyId); // third method to get all tickets of a company
+    }
+}
