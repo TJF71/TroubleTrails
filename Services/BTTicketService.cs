ba72f5bf2fc5074b93c5cb7f353128cedf689caf
TJF71 <138714842+TJF71@users.noreply.github.com>
2024-02-29 14:41:59 -0500

    updates to GetTicketsByRoleAsync

--- Services/BTTicketService.cs
+++ Services/BTTicketService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TroubleTrails.Data;
 using TroubleTrails.Models;
+using TroubleTrails.Models.Enums;
 using TroubleTrails.Services.Interfaces;
 
 namespace TroubleTrails.Services
@@ -181,9 +182,36 @@ namespace TroubleTrails.Services
             throw new NotImplementedException();
         }
 
-        public Task<List<Ticket>> GetTicketsByRoleAsync(string role, string userId, int companyId)
+        public async Task<List<Ticket>> GetTicketsByRoleAsync(string role, string userId, int companyId)
         {
-            throw new NotImplementedException();
+            List<Ticket> tickets = new();
+
+            try
+            {
+                if (role == Roles.Admin.ToString())
+                {
+                    tickets = await GetAllTicketsByCompanyAsync(companyId);
+                } 
+                else if (role == Roles.Developer.ToString())
+                {
+                    tickets = (await GetAllTicketsByCompanyAsync(companyId)).Where(t => t.DeveloperUserId == userId).ToList();
+                }
+                else if (role == Roles.Submitter.ToString())
+                {
+                    tickets = (await GetAllTicketsByCompanyAsync(companyId)).Where(t => t.OwnerUserId == userId).ToList();
+                }
+                else if (role == Roles.ProjectManager.ToString())
+                {
+                    tickets = await GetTicketsByUserIdAsync(userId, companyId);
+                }
+
+                return tickets;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public Task<List<Ticket>> GetTicketsByUserIdAsync(string userId, int companyId)
