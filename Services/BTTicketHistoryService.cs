f22f813d155402115368e0607be1a3a520eddffa
TJF71 <138714842+TJF71@users.noreply.github.com>
2024-03-05 13:49:32 -0500

    added       AddHistoryAsync

---
+++ Services/BTTicketHistoryService.cs
@@ -0,0 +1,45 @@
+﻿using TroubleTrails.Data;
+using TroubleTrails.Models;
+using TroubleTrails.Services.Interfaces;
+
+namespace TroubleTrails.Services
+{
+    public class BTTicketHistoryService : IBTTicketHistoryService
+    {
+        private readonly ApplicationDbContext _context;
+        public BTTicketHistoryService(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+
+        public async Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId)
+        {
+            //new ticket has been added
+            if (oldTicket == null && newTicket != null)
+            {
+                TicketHistory history = new()
+                {
+                    TicketId = newTicket.Id,
+                    Property = "",
+                    OldValue = "",
+                    NewValue = "",
+                    Created = DateTimeOffset.Now,
+                    UserId = userId,
+                    Description = "New Ticket Created"
+                };
+            }
+
+        }
+
+
+        public Task<List<TicketHistory>> GetCompanyTicketsHistoriesAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
