7b5854b84ba688ab66fde92331842ea427495d12
TJF71 <138714842+TJF71@users.noreply.github.com>
2024-03-13 14:43:35 -0400

    made some connection fixes in program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ using TroubleTrails.Services.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = DataUtility.GetConnectionString(builder.Configuration) ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
