5dcf81157dd905eebe6285f743b9f362975c3977
TJF71 <138714842+TJF71@users.noreply.github.com>
2024-04-04 12:20:06 -0400

    created BTUserClaimsPrincipalFactory class

---
+++ Services/Factories/BTUserClaimsPrincipalFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using TroubleTrails.Models;
+
+namespace TroubleTrails.Services.Factories
+{
+    public class BTUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<BTUser, IdentityRole>  //inherits from UserClaimsPrincipalFactory
+    {
+        //constructor
+        public BTUserClaimsPrincipalFactory(UserManager<BTUser> userManager, 
+                                            RoleManager<IdentityRole> roleManager, 
+                                            IOptions<IdentityOptions> optionsAccessor) 
+        : base(userManager, roleManager, optionsAccessor)
+        {
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(BTUser user) //override the GetClaimsAsync method
+        {
+            ClaimsIdentity identity = await base.GenerateClaimsAsync(user); //call the base method
+            identity.AddClaim(new Claim("CompanyId", user.CompanyID.ToString())); //add a claim to the identity object
+            return identity;         
+        }
+    }
+}
