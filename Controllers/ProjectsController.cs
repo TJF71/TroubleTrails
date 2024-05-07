a2c48007bf5be32e38ee939fb5f45661a7244f94
TJF71 <138714842+TJF71@users.noreply.github.com>
2024-05-07 11:47:52 -0400

    Added ViewModel and GET method for AssignMembers

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -99,6 +99,7 @@ namespace TroubleTrails.Controllers
         
         }
 
+        [HttpGet]
         public async Task<IActionResult> AssignPM(int projectId)
         { 
             int companyId = User.Identity.GetCompanyId().Value;
@@ -124,6 +125,31 @@ namespace TroubleTrails.Controllers
            return RedirectToAction(nameof(AssignPM), new {projectId = model.Project.Id});
         
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> AssignMembers(int id)
+        { 
+            ProjectMembersViewModel model = new();
+
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            model.Project = await _projectService.GetProjectByIdAsync(id, companyId);
+
+            List<BTUser> developers = await _rolesService.GetUsersInRoleAsync(nameof(Roles.Developer), companyId);
+            List<BTUser> submitters = await _rolesService.GetUsersInRoleAsync(nameof(Roles.Submitter), companyId);
+
+            List<BTUser> companyMembers = developers.Concat(submitters).ToList();
+
+            List<string> projectMembers = model.Project.Members.Select(m => m.Id).ToList();
+
+            model.Users = new MultiSelectList(companyMembers, "Id", "FullName", projectMembers);
+
+            return View(model); 
+        }
+
+
+
 
 
         // GET: Projects/Details/5
---
+++ Models/ViewModels/ProjectMembersViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TroubleTrails.Models.ViewModels
+{
+    public class ProjectMembersViewModel
+    {
+
+        public  Project? Project { get; set; }
+
+        public  MultiSelectList? Users { get; set; }
+
+        public  List<string>? SelectedUsers { get; set; }
+    
+        
+    
+    }
+}
