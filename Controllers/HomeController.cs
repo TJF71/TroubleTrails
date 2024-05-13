cd246b83bb7bd43a6195ef0810aeed0b52a71c9d
TJF71 <138714842+TJF71@users.noreply.github.com>
2024-05-13 17:12:09 -0400

    added DashBoardViewModel

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Project;
 using System.Diagnostics;
+using TroubleTrails.Extensions;
 using TroubleTrails.Models;
+using TroubleTrails.Models.ViewModels;
+using TroubleTrails.Services.Interfaces;
 
 namespace TroubleTrails.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBTCompanyInfoService _companyInfoService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBTCompanyInfoService companyInfoService)
         {
             _logger = logger;
+            _companyInfoService = companyInfoService;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            DashboardViewModel model = new();
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            model.Company = await _companyInfoService.GetCompanyInfoByIdAsync(companyId);
+            model.Projects = (await _companyInfoService.GetAllProjectsAsync(companyId)).Where(p=>p.Archived==false).ToList();
+            model.Tickets = model.Projects.SelectMany(p=>p.Tickets).Where(t=>t.Archived==false).ToList();   
+            model.Members = model.Company.Members.ToList();
+
+            return View(model);
+
         }
 
         public IActionResult Privacy()
