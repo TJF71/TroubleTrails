63d719f45f7bfc81b283152080cfe56570e349df
TJF71 <138714842+TJF71@users.noreply.github.com>
2024-05-29 12:11:48 -0400

    updates to portfolio dates

---
+++ Data/Migrations/20240528234455_UpdatePortfolioDatesCorrected.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+#nullable disable
+
+namespace TroubleTrails.Data.Migrations
+{
+    public partial class UpdatePortfolioDatesCorrected : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Name",
+                keyValue: "Build a Personal Porfolio",
+                columns: new[] { "StartDate", "EndDate" },
+                values: new object[]
+                {
+                    DateTime.SpecifyKind(new DateTime(2024, 5, 20), DateTimeKind.Utc),
+                    DateTime.SpecifyKind(new DateTime(2024, 5, 20).AddMonths(1), DateTimeKind.Utc)
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Name",
+                keyValue: "Build a Personal Porfolio",
+                columns: new[] { "StartDate", "EndDate" },
+                values: new object[]
+                {
+                    DateTime.SpecifyKind(new DateTime(2021, 8, 20), DateTimeKind.Utc),
+                    DateTime.SpecifyKind(new DateTime(2021, 8, 20).AddMonths(1), DateTimeKind.Utc)
+                });
+        }
+    }
+}
